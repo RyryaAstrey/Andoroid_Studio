4bcee0d64f0fa9efd53148db2ecb75d9ec90e5ef
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-05-02 15:45:48 +0900

    変数について学習

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+        int month; // 変数を宣言
+        month = 9; // 宣言した変数にデータを代入
+        int day = 16; // 変数を初期化（宣言と代入を同時にしている）
+
+        System.out.println("このチャンネルは、");
+        System.out.println(month + "月" + day + "日に公開しました！"); // 変数の参照をしている（処理の中に変数名を入れると参照する）
+    }
+}
