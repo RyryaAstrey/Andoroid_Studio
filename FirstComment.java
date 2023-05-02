33c61de0d6c9f2020a247aaec44d54abb12102eb
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-05-02 14:46:41 +0900

    コメントについて学習

---
+++ FirstComment.java
@@ -0,0 +1,9 @@
+/* FirstComment.java
+ * 文字列を表示するプログラム
+ * 2023.5.2　辻森作成 */
+
+public class FirstComment {
+    public static void main(String[] args) {
+        System.out.println("Javaを楽しもう！"); // 画面表示
+    }
+}
