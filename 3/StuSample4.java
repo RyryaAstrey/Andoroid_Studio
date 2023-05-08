1b05c7808c43456d671b7d1a69300f1fa1794943
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-05-08 13:32:01 +0900

    staticについて学習

---
+++ 3/StuSample4.java
@@ -0,0 +1,11 @@
+class StuSample4 {
+    public static void main(String[] args) {
+        Student4.display();
+
+        Student4 stu1 = new Student4("辻森");
+        Student4.display();
+
+        Student4 stu2 = new Student4("村山");
+        Student4.display();
+    }
+}
