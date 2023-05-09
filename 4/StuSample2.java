27a583767748205eca246e389623005aeb0140c0
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-05-09 10:22:05 +0900

    オーバーライドの学習

---
+++ 4/StuSample2.java
@@ -0,0 +1,8 @@
+public class StuSample2 {
+    public static void main(String[] args) {
+        Student2 stu = new Student2();
+        stu.setName("辻森");
+        stu.setStuNo(1);
+        stu.display();
+    }
+}
