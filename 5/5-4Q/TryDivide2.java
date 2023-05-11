c65f0dab9aa6fbb3e2de817a43b3f5683e7dcf57
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-05-11 15:20:12 +0900

    演習問題5-4Q開始

---
+++ 5/5-4Q/TryDivide2.java
@@ -0,0 +1,8 @@
+public class TryDivide2{
+  public static void main(String[] args){
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.println("a / b = " + (a / b) + " ‚ ‚Ü‚è " + (a % b));
+    System.out.println("ƒvƒƒOƒ‰ƒ€‚ğI—¹‚µ‚Ü‚·");
+  }
+}
